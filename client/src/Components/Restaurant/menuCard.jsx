cac929f98f1526d7dddf7e55a6ee830519dec3cd
pavankpdev <pavankumarkpkp@gmail.com>
2021-05-11 16:30:18 +0530

    add menu page

--- client/src/Components/Restaurant/menuCard.jsx
+++ client/src/Components/Restaurant/menuCard.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import ImageViewer from "react-simple-image-viewer";
+import Viewer from "react-viewer";
 
 const MenuCard = ({ images, category }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const [visible, setVisible] = React.useState(false);
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
   return (
     <>
       <div className="w-5/12 md:w-56">
@@ -9,11 +25,19 @@ const MenuCard = ({ images, category }) => {
             src={images[0]}
             alt={`${category} Menu`}
             className="w-full h-full object-cover rounded-md"
+            onClick={() => setVisible(true)}
           />
         </div>
         <h3 className="font-semibold md:text-xl">{category} Menu</h3>
         <small>{images.length} Pages</small>
       </div>
+      <Viewer
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        images={images.map((image, index) => ({ src: image, alt: index }))}
+      />
     </>
   );
 };
--- client/src/pages/Restaurant/menu.jsx
+++ client/src/pages/Restaurant/menu.jsx
@@ -2,10 +2,33 @@ import React from "react";
 import MenuCard from "../../Components/Restaurant/menuCard";
 
 const MenuRestaurant = () => {
-  return <>
-  
-
-  </>;
+  const menuCard = [
+    {
+      images: [
+        "https://b.zmtcdn.com/data/menus/239/51239/dc68198eb625278a15e9bd403b091248.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+        "https://b.zmtcdn.com/data/menus/239/51239/dc68198eb625278a15e9bd403b091248.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+        "https://b.zmtcdn.com/data/menus/239/51239/dc68198eb625278a15e9bd403b091248.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+      ],
+      category: "Food",
+    },
+    {
+      images: [
+        "https://b.zmtcdn.com/data/menus/239/51239/dc68198eb625278a15e9bd403b091248.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+        "https://b.zmtcdn.com/data/menus/239/51239/dc68198eb625278a15e9bd403b091248.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+        "https://b.zmtcdn.com/data/menus/239/51239/dc68198eb625278a15e9bd403b091248.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+      ],
+      category: "Bar",
+    },
+  ];
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-2">
+        {menuCard.map((menu) => (
+          <MenuCard {...menu} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default MenuRestaurant;
